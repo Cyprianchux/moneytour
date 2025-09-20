1ef434fd64224e22976395cae8a5b92b86e18c79
cyprianchux <cyprianchux@gmail.com>
2025-09-20 17:53:30 +0100

    Added username to localStorage

--- api/scripts/login.js
+++ api/scripts/login.js
@@ -1,36 +1,3 @@
-/*
-document.getElementById("login-form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const username = document.getElementById("username").value.trim();
-  const password = document.getElementById("password").value.trim();
-  const rememberMe = document.getElementById("remember-me").checked;
-
-  let myUserId = null; // store logged in userId
-
-  try {
-    const res = await fetch("http://localhost:5500/api/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password, rememberMe })
-    });
-
-    const userData = await res.json();
-
-    if (userData.success) {
-      console.log(typeof JSON.stringify(localStorage.setItem('myUserId', userData.userId))); // save userId
-      alert("✅ Login successful! Redirecting to your dasboard...");
-      window.location.href = "dashboard.html";
-    } else {
-      alert("❌ Error: " + userData.error);
-    }
-  } catch (err) {
-    console.error("❌ Login request failed:", err);
-    alert("Server error, try again later");
-  }
-});
-*/
-
 document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.getElementById('login-form');
 
@@ -49,12 +16,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         const result = await response.json();
-        console.log(typeof result.userId);
-        console.log(result.userId);
 
         if (response.ok && result.success) {
           // ✅ Save userId in localStorage
           localStorage.setItem('myUserId', JSON.stringify(result.userId));
+          localStorage.setItem('myUsername', username);
 
           alert('Login successful!');
           
